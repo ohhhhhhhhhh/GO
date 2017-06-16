4815979e58891a0cd01bdd08ebc8f6da08185f6a
ohhhhhhhhhh <845680003@qq.com>
2017-06-16 16:23:19 +0800

    Update Go.sh

--- Go.sh
+++ Go.sh
@@ -3,7 +3,7 @@
 #!/bin/sh
 
 #git update
-git checkout dev_1.0
+git checkout 分支名字
 
 if [ $? -ne 0 ]; then
     exit 1
@@ -22,13 +22,12 @@ CONFIG_ROOT_PATH="$(pwd)"
 #删除
 rm -rf "$CONFIG_ROOT_PATH""/build/"
 #清除
-xcodebuild -target RWHMagic clean
+xcodebuild -target 工程名字 clean
 
-xcodebuild archive -workspace RWHMagic.xcworkspace -scheme RWHMagic -configuration Release -archivePath "$CONFIG_ROOT_PATH""/RWHMagic"
+xcodebuild archive -workspace 工程名字.xcworkspace -scheme 工程名字 -configuration Release -archivePath "$CONFIG_ROOT_PATH""/工程名字"
 
-xcodebuild -exportArchive -archivePath "$CONFIG_ROOT_PATH""/RWHMagic.xcarchive"  -exportOptionsPlist "$CONFIG_ROOT_PATH""/RWHMagic/Info.plist" -exportPath "$CONFIG_ROOT_PATH"
+xcodebuild -exportArchive -archivePath "$CONFIG_ROOT_PATH""/工程名字.xcarchive"  -exportOptionsPlist "$CONFIG_ROOT_PATH""/工程名字/Info.plist" -exportPath "$CONFIG_ROOT_PATH"
 
-#cd RWHMagic.ipa
 #上 蒲公英
-curl -F "file=@./RWHMagic.ipa" -F "uKey=bf0184bf9a7b96c9509f5ff614428c53" -F "_api_key=f64387aaebb4b518392a2e1f8353fc5a" https://www.pgyer.com/apiv1/app/upload
+curl -F "file=@./工程名字.ipa" -F "uKey=蒲公英上userKey" -F "_api_key=蒲公英上apiKey" https://www.pgyer.com/apiv1/app/upload
 
